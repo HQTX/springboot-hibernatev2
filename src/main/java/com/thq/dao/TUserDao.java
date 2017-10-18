0c650be1b23b7f8ecad7e93052196f87b58c6966
THQX <254840681@qq.com>
2017-10-18 11:28:00 +0800

    继承分页查询的接口

--- src/main/java/com/thq/dao/TUserDao.java
+++ src/main/java/com/thq/dao/TUserDao.java
@@ -5,38 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+/**
+ * 继承分页查询的接口
+ *JpaSpecificationExecutor
+ */
 @Repository
 public interface TUserDao extends JpaRepository<Tuser,Integer>,JpaSpecificationExecutor<Tuser> {
-    /**
-     *   Jpa里面的方法
-     *   查询全部数据
-     * List<T> findAll();
-
-     List<T> findAll(Sort var1);
-
-     List<T> findAll(Iterable<ID> var1);
-
-     <S extends T> List<S> save(Iterable<S> var1);
-
-     void flush();
-
-     <S extends T> S saveAndFlush(S var1);
-
-     void deleteInBatch(Iterable<T> var1);
-
-     void deleteAllInBatch();
-
-     T getOne(ID var1);
-
-     <S extends T> List<S> findAll(Example<S> var1);
-
-     <S extends T> List<S> findAll(Example<S> var1, Sort var2);
-     * @param userName
-     * @return
-     */
-
     Tuser findByUserName(String userName);
-
-
-
 }
