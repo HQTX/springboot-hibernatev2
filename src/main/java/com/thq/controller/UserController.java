b4ae05201e94d16dfe80f804515285028808bb64
THQX <254840681@qq.com>
2017-10-18 11:55:18 +0800

    添加thymeleaf模版

--- src/main/java/com/thq/controller/UserController.java
+++ src/main/java/com/thq/controller/UserController.java
@@ -1,26 +1,20 @@
 package com.thq.controller;
-
 import com.alibaba.fastjson.JSON;
-import com.thq.entry.Person;
 import com.thq.entry.Tuser;
 import com.thq.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
-
-/*	@RequestMapping("/")
-	public String helloWorld() {
-		return "hello World";
-	}*/
-	
 	/**
 	 * 测试地址  http://localhost:8080/getUser?name=zhao
 	 * @param name
@@ -33,7 +27,9 @@ public class UserController {
 		return JSON.toJSONString(user);
 	}
 	@RequestMapping("/index")
-   public String index1(){
+   public String index1(Map<String,Object> map){
+
+		map.put("hello", "1234");
 		return "home";
    }
 
